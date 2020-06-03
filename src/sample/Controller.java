4e65c5a8aeabed1ba885c1517bebcb6b40f4bf73
dangzed <dangyeuzed@gmail.com>
2020-06-03 22:56:32 +0700

    Almost done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,14 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.util.Random;
 
@@ -14,57 +18,67 @@ public class Controller {
     private GridPane inputArray;
 
     @FXML
-    private GridPane countArray;
-
-    @FXML
     private GridPane sortedArray;
 
     @FXML
-    private Label radix;
+    private Label radixLabel;
 
     @FXML
     void radixSort(ActionEvent event) {
-        for (int radix = 0; radix < 3; radix++) {
-            int[] initialArray = new int[8];
-            int[] indexArray = new int[10];
 
-            for (int i = 0; i < initialArray.length; i++) {
-                initialArray[i] = Integer.parseInt(((Text) inputArray.getChildren().get(i)).getText());
-            }// initialize the array by getting data from element id ='inputArray' from fxml file
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(3), new EventHandler<ActionEvent>() {
+            private int radix = 0;
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                int[] initialArray = new int[8];
+                int[] indexArray = new int[10];
+                radixLabel.setText("Radix : "+ radix);
 
-            for (int i = 0; i < initialArray.length; i++) {
-                // animation, transition code here
-                int nthDigit = getNthDigit(initialArray[i], radix);
-                indexArray[nthDigit]++;
-                ((Text) countArray.getChildren().get(i)).setText(String.valueOf(indexArray[nthDigit]));
-                // animation, transition code here
+                for (int i = 0; i < initialArray.length; i++)
+                    initialArray[i] = Integer.parseInt(getText(inputArray, i).getText());
+
+                for (int item : initialArray) {
+                    int nthDigit = getNthDigit(item, radix);
+                    indexArray[nthDigit]++;
+                }
+                for (int i = 1; i < indexArray.length; i++) indexArray[i] += indexArray[i - 1];
+                for (int i = 9; i >= 1; i--) indexArray[i] = indexArray[i - 1];
+                indexArray[0] = 0;
+
+
+                for (int value : initialArray) {
+                    getText(sortedArray, indexArray[getNthDigit(value, radix)]).setText(String.valueOf(value));
+                    getText(inputArray, indexArray[getNthDigit(value, radix)]).setText(String.valueOf(value));
+                    indexArray[getNthDigit(value, radix)]++;
+                }
+                radix++;
             }
+        }));
+        timeline.setCycleCount(maxRadix(inputArray));
+        timeline.play();
 
-            for (int i = 1; i < indexArray.length; i++) {
-                indexArray[i] += indexArray[i - 1];
-                ((Text) countArray.getChildren().get(i)).setText(String.valueOf(indexArray[i]));
-                // animation, transition code here
-            }
+    }
 
-            for (int i = 9; i >= 1; i--) indexArray[i] = indexArray[i - 1];
-            indexArray[0] = 0;
-            for (int i = 0; i < 10; i++) {
-                ((Text) countArray.getChildren().get(i)).setText(String.valueOf(indexArray[i]));
-                // animation, transition code here
+    public static int maxRadix(GridPane gridPane) {
+        int[] initialArray = new int[8];
+        int max = 0;
+        for (int i = 0; i < initialArray.length; i++)
+            initialArray[i] = Integer.parseInt(getText(gridPane, i).getText());
+        for (int i = 0; i < initialArray.length; i++) {
+            int count = 0;
+            while (initialArray[i] != 0) {
+                initialArray[i] /= 10;
+                count++;
             }
-
-            for (int value : initialArray) {
-                ((Text) sortedArray.getChildren().get(indexArray[getNthDigit(value, radix)])).setText(String.valueOf(value));
-                ((Text) inputArray.getChildren().get(indexArray[getNthDigit(value, radix)])).setText(String.valueOf(value));
-                indexArray[getNthDigit(value, radix)]++;
-            }
+            if (count > max) max = count;
         }
+        return max;
     }
 
     public static int getNthDigit(int element, int radix) {
         for (int i = 0; i < radix; i++) element /= 10;
         return element % 10;
-    } // return digit at specific radix, for example getNthDigit(802,2) return 8
+    }
 
     @FXML
     void resetRandom(ActionEvent event) {
@@ -73,8 +87,10 @@ public class Controller {
             ((Text) inputArray.getChildren().get(i)).setText(String.valueOf(random.nextInt(999)));
             ((Text) sortedArray.getChildren().get(i)).setText("");
         }
-        for (int i = 0; i < 10; i++) {
-            ((Text) countArray.getChildren().get(i)).setText("0");
-        }
+        radixLabel.setText("Radix : ");
+    }
+
+    public static Text getText(GridPane gridPane, int index) {
+        return (Text) gridPane.getChildren().get(index);
     }
 }
